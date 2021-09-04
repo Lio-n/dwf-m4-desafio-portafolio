10613c1165ccb6cc8634119ccbc7e38c8aeef85d
Lion <leonardofontan14@gmail.com>
2021-09-04 18:59:25 -0300

    Add .cont-nav-menu :hover

--- index.js
+++ index.js
@@ -16,3 +16,55 @@
     footerComponent(footerEl);
   };
 })();
+/* 
+<div class="cont-svcs center">
+          <div class="svcs__image"></div>
+          <h3 class="svcs__sub-title">Hago Cosas</h3>
+          <p class="svcs__paragraph">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident,
+            iste perferendis quidem dolorum laudantium, exercitationem hic nemo
+            quibusdam consectetur aliquam rem quisquam maiores cumque tempore
+            repellendus? Quasi fugiat expedita voluptatibus.
+          </p>
+        </div>
+        <div class="cont-svcs center">
+          <div class="svcs__image"></div>
+          <h3 class="svcs__sub-title">Hago Cosas</h3>
+          <p class="svcs__paragraph">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident,
+            iste perferendis quidem dolorum laudantium, exercitationem hic nemo
+            quibusdam consectetur aliquam rem quisquam maiores cumque tempore
+            repellendus? Quasi fugiat expedita voluptatibus.
+          </p>
+        </div>
+        <div class="cont-svcs center">
+          <div class="svcs__image"></div>
+          <h3 class="svcs__sub-title">Hago Cosas</h3>
+          <p class="svcs__paragraph">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident,
+            iste perferendis quidem dolorum laudantium, exercitationem hic nemo
+            quibusdam consectetur aliquam rem quisquam maiores cumque tempore
+            repellendus? Quasi fugiat expedita voluptatibus.
+          </p>
+        </div>
+        <div class="cont-svcs center">
+          <div class="svcs__image"></div>
+          <h3 class="svcs__sub-title">Hago Cosas</h3>
+          <p class="svcs__paragraph">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident,
+            iste perferendis quidem dolorum laudantium, exercitationem hic nemo
+            quibusdam consectetur aliquam rem quisquam maiores cumque tempore
+            repellendus? Quasi fugiat expedita voluptatibus.
+          </p>
+        </div>
+        <div class="cont-svcs center">
+          <div class="svcs__image"></div>
+          <h3 class="svcs__sub-title">Hago Cosas</h3>
+          <p class="svcs__paragraph">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident,
+            iste perferendis quidem dolorum laudantium, exercitationem hic nemo
+            quibusdam consectetur aliquam rem quisquam maiores cumque tempore
+            repellendus? Quasi fugiat expedita voluptatibus.
+          </p>
+        </div>
+*/
